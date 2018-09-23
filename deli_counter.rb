116f59559608b64341825c0fef0eceb13f1b5f6a
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-23 21:39:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(list)
+  if list.length == 0
+    puts "The line is currently empty."
+  else
+      status = "The line is currently:"
+      list.each_with_index do |name, index|
+      status += " #{index.to_i + 1}. #{name}.
+    end
+      puts #{status}
+  end
+end
